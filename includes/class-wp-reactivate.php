0b191dc4eff8f1804ec45a17960e2fe6ee7cda1c
Domenico Nusca <domenico.nusca@gmail.com>
2017-05-03 13:11:07 +0200

    base plugin class

---
+++ includes/class-wp-reactivate.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * WP-Reactivate
+ *
+ *
+ * @package   WP-Reactivate
+ * @author    Pangolin
+ * @license   GPL-3.0
+ * @link      https://gopangolin.com
+ * @copyright 2017 Pangolin (Pty) Ltd
+ */
+
+/**
+ * @package WP-Reactivate
+ */
+class WPReactivate {
+
+	/**
+	 * Plugin version, used for cache-busting of style and script file references.
+	 *
+	 * @since   1.0.0
+	 *
+	 * @var     string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * The variable name is used as the text domain when internationalizing strings
+	 * of text. Its value should match the Text Domain file header in the main
+	 * plugin file.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $plugin_slug = 'wp-reactivate';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin by setting localization and loading public scripts
+	 * and styles.
+	 *
+	 * @since     1.0.0
+	 */
+	private function __construct() {
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+	}
+
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+}

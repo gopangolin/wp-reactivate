11f369c230e4a8fbbc41dae724d598659f5bda7b
Domenico Nusca <domenico.nusca@gmail.com>
2017-05-03 12:56:20 +0200

    base plugin file

---
+++ wp-reactivate.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * WP-Reactivate
+ *
+ *
+ * @package   WP-Reactivate
+ * @author    Pangolin
+ * @license   GPL-3.0
+ * @link      https://gopangolin.com
+ * @copyright 2017 Pangolin (Pty) Ltd
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP-Reactivate
+ * Plugin URI:        https://gopangolin.com
+ * Description:       React boilerplate for WordPress plugins
+ * Version:           0.1.0
+ * Author:            pangolin
+ * Author URI:        https://gopangolin.com
+ * Text Domain:       wp-reactivate
+ * License:           GPL-3.0
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-wp-reactivate.php' );
+
+
+/*
+ * Register activation and deactivation hooks
+ */
+register_activation_hook( __FILE__, array( 'WPReactivate', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'WPReactivate', 'deactivate' ) );
+
+
+add_action( 'plugins_loaded', array( 'WPReactivate', 'get_instance' ) );
